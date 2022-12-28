0089975415319ed8e62d375b85316ac5ec83cc83
domin <dominicentekgaming@gmail.com>
2022-12-28 17:51:26 +0100

    Add 4-4 and 4-5

--- desktop/src/com/smj/game/score/StaticScore.java
+++ desktop/src/com/smj/game/score/StaticScore.java
@@ -4,7 +4,7 @@ public class StaticScore extends Score {
     public static final StaticScore BRICK = new StaticScore(50);
     public static final StaticScore COIN = new StaticScore(200);
     public static final StaticScore POWERUP = new StaticScore(1000);
-    public static final StaticScore TIMER = new StaticScore(50);
+    public static final StaticScore TIMER = new StaticScore(5);
     public static final StaticScore SHELL_KICK = new StaticScore(400);
     public static final StaticScore CHECKPOINT = new StaticScore(2000);
     public static final StaticScore FIREBALL = new StaticScore(100);
