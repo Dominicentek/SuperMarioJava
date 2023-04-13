98178b838af44e6e100d713b5f003938b6fff912
domin <dominicentekgaming@gmail.com>
2023-04-13 14:35:19 +0200

    Fix crates

--- desktop/src/com/smj/game/entity/behavior/CrateBehavior.java
+++ desktop/src/com/smj/game/entity/behavior/CrateBehavior.java
@@ -7,11 +7,18 @@ public class CrateBehavior implements EntityBehavior {
     public void update(GameEntity entity, GameLevel level) {
         if (level.fluid == null) return;
 
-        // the physics engine doesnt like this solution but fuck it
+        // i had to fight with the physics engine for this solution but it works so :p
         if (level.fluid.movement.getFluidLevel() * 100 / 16 < entity.getPhysics().getHitbox().y + 100) {
             entity.getPhysics().getConfig().gravity = 0;
             entity.getPhysics().getConfig().underwaterGravity = 0;
             entity.getPhysics().getHitbox().y = level.fluid.movement.getFluidLevel() * 100 / 16 - 50;
+            int tileX = entity.getPhysics().getHitbox().x / 100;
+            int tileY = entity.getPhysics().getHitbox().y / 100;
+            while (level.getTileList().get(level.getTileAt(tileX, tileY)).isSolid()) {
+                entity.getPhysics().getHitbox().y = (tileY + 1) * 100;
+                tileX = entity.getPhysics().getHitbox().x / 100;
+                tileY = entity.getPhysics().getHitbox().y / 100;
+            }
         }
         else {
             entity.getPhysics().getConfig().gravity = level.getPhysicsConfig().gravity;
