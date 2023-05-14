7fa72d9af0da1d853135c3172c4f85da2ef7a4c3
Dominicentek <dominicentekgaming@gmail.com>
2023-05-14 15:04:09 +0200

    Make two colliding shells kill each other

--- desktop/src/com/smj/game/entity/behavior/ShellVulnerableBehavior.java
+++ desktop/src/com/smj/game/entity/behavior/ShellVulnerableBehavior.java
@@ -10,13 +10,22 @@ import com.smj.util.AudioPlayer;
 
 public class ShellVulnerableBehavior implements EntityBehavior {
     public void onEntityCollide(GameEntity entity, GameLevel level, GameEntity collider) {
-        if (collider.entityType == EntityType.GREEN_KOOPA_SHELL || collider.entityType == EntityType.RED_KOOPA_SHELL || collider.entityType == EntityType.BUZZY_BEETLE_SHELL) {
+        if (isShell(collider.entityType)) {
             if (collider.getBehavior(ShellBehavior.class).speedFactor != 0) {
                 Game.awardScore(collider.score, Location.entity(collider));
                 level.getEntityManager().unloadEntity(entity);
                 Game.particles.add(new FallingEnemyParticle(entity));
                 AudioPlayer.KICK.play(Location.entity(entity));
             }
+            if (isShell(entity.entityType)) {
+                Game.awardScore(entity.score, Location.entity(entity));
+                level.getEntityManager().unloadEntity(collider);
+                Game.particles.add(new FallingEnemyParticle(collider));
+                AudioPlayer.KICK.play(Location.entity(collider));
+            }
         }
+    }
+    public boolean isShell(EntityType type) {
+        return type == EntityType.GREEN_KOOPA_SHELL || type == EntityType.RED_KOOPA_SHELL || type == EntityType.BUZZY_BEETLE_SHELL;
     }
 }
